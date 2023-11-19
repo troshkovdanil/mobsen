78a08237b4321928a0dce3d31400b0c89cc404a4
Daniil Troshkov <troshkovdanil@gmail.com>
2023-11-19 17:39:11 +0400

    test button

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.text.Layout
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.ComponentActivity
@@ -32,6 +33,32 @@ class MainActivity : ComponentActivity() {
         val btn_https = findViewById<Button>(R.id.btn_https)
         btn_https.setOnClickListener(){
             CoroutineScope(Dispatchers.Default).async { httpsConnect(txt = txt) }
+        }
+
+        val btn_test = findViewById<Button>(R.id.btn_test)
+        var cntr = 0
+        btn_test.setOnClickListener(){
+            val text: String = txt.text.toString()
+            var start = 0
+            var end = 0
+            var is_num = true
+            do {
+                end = text.indexOf("\n", startIndex = start)
+                if (end != -1) {
+                    is_num = text.substring(start,end).matches(Regex("[0-9]*"))
+                    if (!is_num) {
+                        cntr = 0;
+                        txt.text = "0"
+                        break
+                    }
+                    start = end + 1
+                }
+            } while (end != -1)
+            if (is_num) {
+                cntr++
+                txt.append("\n")
+                txt.append(cntr.toString())
+            }
         }
     }
 }
