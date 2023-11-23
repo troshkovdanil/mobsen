6405bf97403197d88fe6ed0e3b5bc5340ec1333d
Daniil Troshkov <troshkovdanil@gmail.com>
2023-11-23 23:25:57 +0400

    get location and post telemetry in threads

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.myapplication
 
 import android.Manifest
-import android.app.ActivityManager
 import android.content.Context
 import android.content.pm.PackageManager
 import android.location.Location
@@ -9,81 +8,24 @@ import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Bundle
 import android.text.method.ScrollingMovementMethod
-import android.widget.Button
+import android.view.View
 import android.widget.TextView
+import android.widget.ToggleButton
 import androidx.activity.ComponentActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import kotlinx.coroutines.*
-import java.net.URL
 import org.json.JSONObject
-import java.io.BufferedReader
 import java.io.BufferedWriter
-import java.io.IOException
-import java.io.InputStreamReader
 import java.io.OutputStream
 import java.io.OutputStreamWriter
 import java.net.HttpURLConnection
+import java.net.URL
 import java.net.URLEncoder
 import javax.net.ssl.HttpsURLConnection
 
 
-fun httpConnect(txt: TextView) {
-    txt.text = try { URL("http://www.example.com/").readText() } catch (e: Exception) { e.toString() }
-}
-@Throws(IOException::class)
-private fun encodeParams(params: JSONObject): String? {
-    val result = StringBuilder()
-    var first = true
-    val itr = params.keys()
-    while (itr.hasNext()) {
-        val key = itr.next()
-        val value = params[key]
-        if (first) first = false else result.append("&")
-        result.append(URLEncoder.encode(key, "UTF-8"))
-        result.append("=")
-        result.append(URLEncoder.encode(value.toString(), "UTF-8"))
-    }
-    return result.toString()
-}
-
-fun httpsConnect(txt: TextView) {
-    //txt.text = try { URL("https://tls13.1d.pw/").readText() } catch (e: Exception) { e.toString() }
-    //txt.text = try { URL("https://tls13.akamai.io/").readText() } catch (e: Exception) { e.toString() }
-    val d = try { URL("https://ptsv3.com/t/gps_dt/d/").readText() } catch (e: Exception) { e.toString() }
-    if (d != "[]")
-        return
-    val postDataParams = JSONObject()
-    postDataParams.put("AuthUsername", "")
-    postDataParams.put("AuthPassword", "")
-    postDataParams.put("ResponseCode", "200")
-    postDataParams.put("ResponseBody", txt.text)
-    postDataParams.put("ResponseDelay", 0)
-    val url = URL("https://ptsv3.com/t/gps_dt/edit/")
-    val conn: HttpURLConnection = url.openConnection() as HttpURLConnection
-    conn.readTimeout = 3000
-    conn.connectTimeout = 3000
-    conn.requestMethod = "POST"
-    conn.doInput = true
-    conn.doOutput = true
-    val os: OutputStream = conn.outputStream
-    val writer = BufferedWriter(OutputStreamWriter(os, "UTF-8"))
-    writer.write(encodeParams(postDataParams))
-    writer.flush()
-    writer.close()
-    os.close()
-    txt.text = "Posting..."
-    val responseCode: Int = conn.responseCode // To Check for 200
-    txt.text = responseCode.toString()
-    if (responseCode == HttpsURLConnection.HTTP_OK) {
-        txt.text = "Posting...OK"
-    }
-}
-
 class MainActivity : ComponentActivity(), LocationListener {
-    private lateinit var locationManager: LocationManager
-    private val locationPermissionCode = 2
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -93,47 +35,10 @@ class MainActivity : ComponentActivity(), LocationListener {
         val txt = findViewById<TextView>(R.id.txtview)
         txt.movementMethod = ScrollingMovementMethod()
         txt.text = ""
-
-        val btn_gps = findViewById<Button>(R.id.btn_gps)
-        btn_gps.setOnClickListener(){
-            getLocation()
-        }
-
-        val btn_https = findViewById<Button>(R.id.btn_https)
-        btn_https.setOnClickListener(){
-            CoroutineScope(Dispatchers.Default).async { httpsConnect(txt = txt) }
-        }
-
-        val btn_clear = findViewById<Button>(R.id.btn_clear)
-        //var cntr = 0
-        btn_clear.setOnClickListener(){
-            txt.text = ""
-            /*
-            val text: String = txt.text.toString()
-            var start = 0
-            var end = 0
-            var is_num = true
-            do {
-                end = text.indexOf("\n", startIndex = start)
-                if (end != -1) {
-                    is_num = text.substring(start,end).matches(Regex("[0-9]*"))
-                    if (!is_num) {
-                        cntr = 0;
-                        txt.text = "0"
-                        break
-                    }
-                    start = end + 1
-                }
-            } while (end != -1)
-            if (is_num) {
-                cntr++
-                txt.append("\n")
-                txt.append(cntr.toString())
-            }
-            */
-        }
     }
 
+    private lateinit var locationManager: LocationManager
+    private val locationPermissionCode = 2
     private fun getLocation() {
         locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
         if ((ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
@@ -141,23 +46,98 @@ class MainActivity : ComponentActivity(), LocationListener {
         }
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5f, this)
     }
+
     override fun onLocationChanged(location: Location) {
+        val telemetry = location.time.toString() + "," +
+                location.latitude.toString() + "," +
+                location.longitude.toString() + "\n"
         val txt = findViewById<TextView>(R.id.txtview)
-        txt.text = location.time.toString() + "," +
-                   location.latitude.toString() + "," +
-                   location.longitude.toString() + "\n"
+        txt.text = telemetry
     }
 
-    /*
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        if (requestCode == locationPermissionCode) {
-            val txt = findViewById<TextView>(R.id.txtview)
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                txt.text = "Permission Granted"
-            } else {
-                txt.text = "Permission Denied"
+    private lateinit var gpsJob: Job
+    fun onGPSToggleClicked(view: View) {
+        val on = (view as ToggleButton).isChecked
+        if (on) {
+            gpsJob = CoroutineScope(Dispatchers.Main,).launch {
+                while (true) {
+                    yield()
+                    val txt = findViewById<TextView>(R.id.txtview)
+                    val telemetry = txt.text.toString()
+                    if (telemetry == "") {
+                        getLocation()
+                    }
+                }
             }
+        } else {
+            gpsJob.cancel()
         }
     }
-    */
+
+    private fun encodeParams(params: JSONObject): String? {
+        val result = StringBuilder()
+        var first = true
+        val itr = params.keys()
+        while (itr.hasNext()) {
+            val key = itr.next()
+            val value = params[key]
+            if (first) first = false else result.append("&")
+            result.append(URLEncoder.encode(key, "UTF-8"))
+            result.append("=")
+            result.append(URLEncoder.encode(value.toString(), "UTF-8"))
+        }
+        return result.toString()
+    }
+    private fun postTelemetry(txt: TextView, telemetry: String) {
+        val postDataParams = JSONObject()
+        postDataParams.put("AuthUsername", "")
+        postDataParams.put("AuthPassword", "")
+        postDataParams.put("ResponseCode", "200")
+        postDataParams.put("ResponseBody", telemetry)
+        postDataParams.put("ResponseDelay", 0)
+        val url = URL("https://ptsv3.com/t/gps_dt/edit/")
+        val conn: HttpURLConnection = url.openConnection() as HttpURLConnection
+        conn.readTimeout = 3000
+        conn.connectTimeout = 3000
+        conn.requestMethod = "POST"
+        conn.doInput = true
+        conn.doOutput = true
+        val os: OutputStream = conn.outputStream
+        val writer = BufferedWriter(OutputStreamWriter(os, "UTF-8"))
+        writer.write(encodeParams(postDataParams))
+        writer.flush()
+        writer.close()
+        os.close()
+        val responseCode: Int = conn.responseCode
+        if (responseCode == HttpsURLConnection.HTTP_OK) {
+            txt.text = ""
+        }
+    }
+
+    private lateinit var httpsJob: Job
+    fun onHTTPSToggleClicked(view: View) {
+        val on = (view as ToggleButton).isChecked
+        if (on) {
+            httpsJob = CoroutineScope(Dispatchers.Default).launch {
+                while (true) {
+                    yield()
+                    val txt = findViewById<TextView>(R.id.txtview)
+                    val telemetry = txt.text.toString()
+                    if (telemetry == "") {
+                        continue
+                    }
+                    val dump = try {
+                        URL("https://ptsv3.com/t/gps_dt/d/").readText()
+                    } catch (e: Exception) {
+                        e.toString()
+                    }
+                    if (dump == "[]") {
+                        postTelemetry(txt, telemetry)
+                    }
+                }
+            }
+        } else {
+            httpsJob.cancel()
+        }
+    }
 }
